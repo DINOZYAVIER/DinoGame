9acf2a92ecbfb141581da29088c33face72f3388
Arthur Konovalov <dinozyavier@outlook.com>
2020-08-17 18:05:12 +0300

    Signed-off-by: Arthur Konovalov <dinozyavier@outlook.com>
    Implemented player movement capability.

--- source/main.cpp
+++ source/main.cpp
@@ -9,10 +9,13 @@ const int default_amount_of_enemies = 3;
 const int default_amount_of_bullets = 3;
 const int safe_margin = 150;
 
+const int AMOUNT_OF_SPRITES = 4;
 const int PLAYER_SPRITE = 0;
 const int ENEMY_SPRITE = 1;
 const int BULLET_SPRITE = 2;
 const int BACKGROUND_SPRITE = 3;
+
+const int PLAYER_SPEED = 20;
 
 class MyFramework : public Framework {
 
@@ -36,7 +39,9 @@ public:
 		}
 
 		Entities (MyFramework& fr) 
-		{
+		{			
+			enemies_quantity = fr.amount_of_enemies;
+			bullets_quantity = fr.amount_of_bullets;
 
 			//array of enemies' positions
 			enemies_pos = new int* [fr.amount_of_enemies];
@@ -49,6 +54,17 @@ public:
 				enemies_pos[i] = new int [2];
 		}
 
+		~Entities() {
+
+		//	for (int i = 0; i < enemies_quantity; ++i)
+		//		delete [] enemies_pos[i];
+		//	delete enemies_pos;
+
+		//	for (int i = 0; i < bullets_quantity; ++i)
+		//		delete [] bullets_pos[i];
+		//	delete bullets_pos;
+
+		}
 		void PositionFill(MyFramework& fr)
 		{
 
@@ -57,6 +73,12 @@ public:
 			//player's position
 			player_pos[0][0] = (fr.xres - spriteSize[PLAYER_SPRITE][0]) / 2;
 			player_pos[0][1] = (fr.yres - spriteSize[PLAYER_SPRITE][1]) / 2;
+
+			dx = 0;
+			dy = 0;
+
+			xlimit = (fr.mapsize_x - fr.xres) / 2;
+			ylimit = (fr.mapsize_y - fr.yres) / 2;
 
 			//here we fill enemies' position
 			for (int i = 0; i < fr.amount_of_enemies; ++i)
@@ -73,7 +95,6 @@ public:
 						break;
 					else
 						continue;
-
 				} while (1);
 			}
 		}
@@ -92,14 +113,98 @@ public:
 				for (int j = 0; j < fr.mapsize_y; j += spriteSize[BACKGROUND_SPRITE][1])
 					drawSprite(spriteArray[BACKGROUND_SPRITE], i, j);
 		}
+
+		void playerMove (MyFramework& fr, FRKey k) {
+
+			switch(k)
+			{
+				case FRKey::LEFT:
+
+					if (dx > -xlimit)
+					{
+						for (int i = 0; i < enemies_quantity; ++i)
+							enemies_pos[i][0] += PLAYER_SPEED;
+				
+						dx -= PLAYER_SPEED;
+					}
+					else
+					{
+						for (int i = 0; i < enemies_quantity; ++i)
+							enemies_pos[i][0] -= xlimit + dx;
+						dx = -xlimit;
+					}	
+					break;
+
+				case FRKey::RIGHT:
+
+					if (dx < xlimit)
+					{
+						for (int i = 0; i < enemies_quantity; ++i)
+							enemies_pos[i][0] -= PLAYER_SPEED;
+				
+						dx += PLAYER_SPEED;
+					}
+					else
+					{
+						for (int i = 0; i < enemies_quantity; ++i)
+							enemies_pos[i][0] += dx - xlimit;
+						dx = xlimit;
+					}
+					break;
+
+				case FRKey::DOWN:
+
+					if (dy < ylimit)
+					{
+						for (int i = 0; i < enemies_quantity; ++i)
+							enemies_pos[i][1] -= PLAYER_SPEED;
+				
+						dy += PLAYER_SPEED;
+					}
+					else
+					{
+						for (int i = 0; i < enemies_quantity; ++i)
+							enemies_pos[i][1] -= dy - ylimit;
+						dy = ylimit;
+					}
+					break;
+
+				case FRKey::UP:
+					if (dy > -ylimit)
+					{
+						for (int i = 0; i < enemies_quantity; ++i)
+							enemies_pos[i][1] += PLAYER_SPEED;
+				
+						dy -= PLAYER_SPEED;
+					}
+					else
+					{
+						for (int i = 0; i < enemies_quantity; ++i)
+							enemies_pos[i][1] += ylimit + dy;
+						dy = -ylimit;
+					}
+					break;
+
+				default:
+					break;
+			}
+
+
+		}
 	private:
 		//here I try to implement data oriented design (structure of arrays instead of array of structures) 
 		int player_pos[1][2];
 		int **enemies_pos;
 		int **bullets_pos;
-		Sprite* spriteArray[4];
-		int spriteSize[4][2];
-
+		Sprite* spriteArray[AMOUNT_OF_SPRITES];
+		int spriteSize[AMOUNT_OF_SPRITES][2];
+		int dx;
+		int dy;
+		int xlimit;
+		int ylimit;
+		int enemies_quantity;
+		int bullets_quantity;
+		bool moveFlag[4];
 	};
 
     void ParamCheck (int argc, char *argv[]);
@@ -126,7 +231,7 @@ public:
 		amount_of_bullets = default_amount_of_bullets;
 	}
 
-	ent = new Entities(*this);	
+	ent = new Entities(*this);
     }
 
 
@@ -141,23 +246,21 @@ public:
         virtual bool Init() {
 
 		ent->PositionFill(*this);
-		//ent->SpriteInit();
-
-            return true;
+            	return true;
         }
     
         virtual void Close() {
-    
+		//delete ent; 
         }
     
         virtual bool Tick() {
 		ent->makeBackground(*this);
 		ent->DisplayEntities(*this);
-            	return false;
+            	return ShouldStop;
         }
     
         virtual void onMouseMove(int x, int y, int xrelative, int yrelative) {
-    
+
         }
     
         virtual void onMouseButtonClick(FRMouseButton button, bool isReleased) {
@@ -165,7 +268,10 @@ public:
         }
     
         virtual void onKeyPressed(FRKey k) {
-        }
+
+		ent->playerMove(*this, k);
+			
+	}
     
         virtual void onKeyReleased(FRKey k) {
 	}
@@ -193,6 +299,7 @@ private:
     int amount_of_enemies = 0;
     int amount_of_bullets = 0;
     Entities *ent;
+    bool ShouldStop = false;
 };
     
 
